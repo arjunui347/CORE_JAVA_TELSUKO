741263c5b2a95f0926f6dff2855831ac9e916f39
DELL <arjunui347@gmail.com>
2026-03-03 18:45:53 +0530

    This and Super Method Classes

---
+++ src/Tel_51_Parent_Or_Super.java
@@ -0,0 +1,13 @@
+public class Tel_51_Parent_Or_Super {
+
+    public Tel_51_Parent_Or_Super() {
+        super();//Comes under Scenario-02 and Scenario-05 of class: Tel_51_This_And_Super_Method.
+        System.out.println("Default Constructor of Class: Tel_51_Parent_Or_Super");
+    }
+
+    public Tel_51_Parent_Or_Super(int n) {
+        super();//Comes under Scenario-02 and scenario-05 of class: Tel_51_This_And_Super_Method.
+        System.out.println("Parameterized Constructor of Class: Tel_51_Parent_Or_Super");
+    }
+
+}
